44bc56e7de85f3e7a2bacc57928c8550b0220739
madhuy <madhuy@gendevs.com>
2020-12-31 11:23:17 +0530

    pakages updated to angular 11

--- src/app/dialogs/zoomed-image/zoomed-image.component.spec.ts
+++ src/app/dialogs/zoomed-image/zoomed-image.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ZoomedImageComponent } from './zoomed-image.component';
 
@@ -6,7 +6,7 @@ describe('ZoomedImageComponent', () => {
   let component: ZoomedImageComponent;
   let fixture: ComponentFixture<ZoomedImageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ZoomedImageComponent ]
     })
