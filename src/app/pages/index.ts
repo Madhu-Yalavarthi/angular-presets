803b169fbee36ca6ae12727a41187e81fef07877
madhuy <madhuy@gendevs.com>
2021-01-13 11:34:27 +0530

    page not found added

--- src/app/pages/index.ts
+++ src/app/pages/index.ts
@@ -1,8 +1,10 @@
 import { BackgroundWallPaperComponent } from './background-wall-paper/background-wall-paper.component';
 import { CustomDialogComponent } from './custom-dialog/custom-dialog.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 export const Pages = [
   CustomDialogComponent,
-  BackgroundWallPaperComponent
+  BackgroundWallPaperComponent,
+  PageNotFoundComponent
 ]
