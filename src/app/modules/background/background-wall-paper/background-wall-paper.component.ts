37a1f8a365f3e0bc306e212350ea3246fee48041
madhuy <madhuy@gendevs.com>
2021-01-13 15:14:49 +0530

    active class added for background

--- src/app/modules/background/background-wall-paper/background-wall-paper.component.ts
+++ src/app/modules/background/background-wall-paper/background-wall-paper.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BackgroundWallPaperComponent implements OnInit {
   REACT_ICON = "assets/svg/react.svg";
-  view = "waves";
+  view = "square";
   options = ["square", "bubbles", "waves"];
   constructor() { }
 
